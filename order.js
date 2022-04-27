783f52821268a0c5e6759636b7adfa1301bb7999
Valentina <maggio.vale@gmail.com>
2022-04-27 18:50:12 +0100

    Refactor getPrice method

--- order.js
+++ order.js
@@ -10,21 +10,18 @@ class Order {
     return `Added: ${item}, $2.5`;
   }
 
-  getShopDetails() {
-    const jsonString = fs.readFileSync('./hipstercoffee.json');
-    const info = JSON.parse(jsonString);
-    console.log("printing 1")
-    console.log(info);
-
-    return info;
-  }
-
-  getPrice() {
-    const shopDetails = this.getShopDetails();
-    const price = shopDetails[0].prices[0]["Tiramisu"];
+  getPrice(item) {
+    const shopDetails = this.#getShopDetails();
+    const price = shopDetails[0].prices[0][`${item}`];
     console.log(price);
 
     return 11.40;
+  }
+
+  #getShopDetails() {
+    const jsonString = fs.readFileSync('./hipstercoffee.json');
+    const info = JSON.parse(jsonString);
+    return info;
   }
 }
 
