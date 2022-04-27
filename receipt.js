db9c5168fbc9474545672f115b717a7c117eff62
Valentina <maggio.vale@gmail.com>
2022-04-27 21:53:01 +0100

    Add method in Receipt class to return list of items with prices

--- receipt.js
+++ receipt.js
@@ -1,5 +1,7 @@
 class Receipt {
-  
+  getItems(orderList) {
+    return 'Americano    1 x 3.75';
+  }
 }
 
 module.exports = Receipt;
--- receipt.test.js
+++ receipt.test.js
@@ -6,4 +6,10 @@ describe('Receipt', () => {
 
     expect(receipt).toBeInstanceOf(Receipt);
   });
+
+  it('returns the items in the order with prices and quantities', () => {
+    const receipt = new Receipt();
+
+    expect(receipt.printItems(['Americano', 3.75])).toBe('Americano    1 x 3.75');
+  });
 });
